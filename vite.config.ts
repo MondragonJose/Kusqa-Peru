5d83894a95680c67f2cbce35c4ce73380875b5b4
MondragonJose <mondragonlujan@gmail.com>
2026-05-26 14:02:33 -0500

    fix: restore correct tanstack react-start vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/start/plugin/vite"
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
